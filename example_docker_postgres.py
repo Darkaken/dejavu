0626116256ab0c5175f63f0b0fb3e6a6d84aedbb
Cristian <cfazio@uc.cl>
2022-07-26 09:56:29 +0000

    update estandarizacion

--- example_docker_postgres.py
+++ example_docker_postgres.py
@@ -1,9 +1,12 @@
+import audioop
 import json
 
 from dejavu import Dejavu
 from dejavu.logic.recognizer.file_recognizer import FileRecognizer
 from time import time
 import os
+
+from pydub import AudioSegment
 
 # load config from a JSON file (or anything outputting a python dictionary)
 config = {
@@ -24,11 +27,41 @@ if __name__ == '__main__':
     # Fingerprint all the mp3's in the directory we give it
 
     a = time()
-    djv.fingerprint_directory("test", [".mp3"])
 
-    # Recognize audio from a file
+    ### PARA HACER HUELLAS DE LAS TANDAS, SOLO UNA VEZ
+
+    directorio_tandas = "test"
+
+    for filename in os.listdir(directorio_tandas):
+        audiofile = AudioSegment.from_file(os.path.join(directorio_tandas, filename))
+
+        quality = 44100 / (2 * audiofile.frame_rate)
+        audiofile = audiofile.set_frame_rate(int(audiofile.frame_rate * quality))
+
+        audiofile.export(os.path.join(directorio_tandas, filename), format="mp3")
+
+    djv.fingerprint_directory(directorio_tandas, [".mp3"])
+
+    ##############################################
+
     b = time()
-    results = djv.recognize(FileRecognizer, "mp3/tanda_4.mp3")
+    # Recognize audio from a file
+
+    to_recognize = "mp3/tanda_4.mp3"
+
+    try:
+        audiofile = AudioSegment.from_file(to_recognize)
+
+        quality = 44100 / (2 * audiofile.frame_rate)
+        audiofile = audiofile.set_frame_rate(int(audiofile.frame_rate * quality))
+
+        audiofile.export(to_recognize, format="mp3")
+
+    except audioop.error:
+        print("Error en estandarizacion de calidad")
+
+
+    results = djv.recognize(FileRecognizer, to_recognize)
     print(f'Tiempo de identificacion: {time() - b} seg')
 
     return_vector = [[x, 0] for x in os.listdir('test')]
